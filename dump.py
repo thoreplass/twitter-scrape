670a68ffe2e4dd0913144b2a4a48fe07d7a1e8eb
Knut Ole Thoreplass <knut.ole.thoreplass@gmail.com>
2020-10-04 13:53:39 +0200

    First build
    First build

--- dump.py
+++ dump.py
@@ -2,8 +2,9 @@ import settings
 import tweepy
 import dataset
 from textblob import TextBlob
+from datafreeze import freeze
 
 db = dataset.connect(settings.CONNECTION_STRING)
 
 result = db[settings.TABLE_NAME].all()
-dataset.freeze(result, format='csv', filename=settings.CSV_NAME)
+freeze(result, format='csv', filename=settings.CSV_NAME)
--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
-TRACK_TERMS = ["trump", "clinton", "sanders", "hillary clinton", "bernie", "donald trump"]
-CONNECTION_STRING = ""
+TRACK_TERMS = ["trump", "biden", "donald trump", "joe biden"]
+CONNECTION_STRING = "sqlite:///tweets.db"
 CSV_NAME = "tweets.csv"
 TABLE_NAME = "election"
 
 try:
-    from private import *
+    from private import TWITTER_KEY, TWITTER_SECRET, TWITTER_APP_KEY, TWITTER_APP_SECRET
 except Exception:
     pass
